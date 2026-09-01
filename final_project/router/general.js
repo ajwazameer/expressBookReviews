da87c6d0c1830098ab22182cb1738f53048cbc17
ajwazameer <ajwazameer2004@gmail.com>
2026-09-01 15:05:16 +0500

    Task 3 completed: get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,12 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const booksArray = Object.values(books);
+  const book = booksArray.filter((book) => {
+    return book.author === author;
+  });
+  return res.status(200).json(book);
 });
 
 // Get all books based on title
